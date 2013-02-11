74efaae6b70cc3f83404c77da01da25b6db3bd37
Yasunori Suzuki <railsyas@gmail.com>
2013-02-11 15:28:55 +0900

    [Diffferences] + Created dropbox_uploader.
    [Reasons]
    + Handson of dropbox API.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DropboxUploader::Application.config.secret_token = '12743b04b22050157d6b458e7e5d3000b956067eebc736b980a73e93c8e0fffafe1133b188201806c8420b00b2edddab98230d35540fc0cafe89aadf803d7883'
